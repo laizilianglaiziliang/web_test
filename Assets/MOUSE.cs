f30904838a20e82eeb63464563014c9162c7b060
lzl <lzil@bupt.edu.cn>
2020-01-06 16:53:04 +0800

    init success

---
+++ Assets/MOUSE.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MOUSE : MonoBehaviour {
+   public GameObject a;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            a.transform.localScale = new Vector3(0, 0, 0);
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            a.transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
+}
